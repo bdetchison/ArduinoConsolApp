b5cb6dfca4f46548e2604c99ea35c62b6c8460ff
bdetchison <bdetchison@gmail.com>
2016-06-08 23:04:25 -0400

    Add integration with database

--- ArduinoConsoleApp/ArduinoConsoleApp/Program.cs
+++ ArduinoConsoleApp/ArduinoConsoleApp/Program.cs
@@ -2,20 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
 using System.Threading.Tasks;
 using System.IO.Ports;
 using System.IO;
 using System.Threading;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace ArduinoConsoleApp
 {
     class Program
     {
         private static int showLight = 0;
-     
+        private static string connectionString;
 
         static void Main(string[] args)
         {
+            using (StreamReader sr = new StreamReader(ConfigurationManager.AppSettings.Get("connStringFile")))
+            {
+                // Read the stream to a string, and write the string to the console.
+                connectionString = sr.ReadToEnd();
+            }
+
+
+
             SerialPort serialPort = new SerialPort();
             serialPort.BaudRate = 9600;
             serialPort.PortName = "COM5";
@@ -60,12 +71,50 @@ namespace ArduinoConsoleApp
             try
             {
                 SerialPort serialPort = (SerialPort)sender;
-                Console.Write(serialPort.ReadLine() + Environment.NewLine);
+                var data = serialPort.ReadLine();
+                float result;
+
+                Console.Write(data + Environment.NewLine);
+
+                data = data.Replace(" ", "").Replace("\r", "");
+                var splitData = data.Split(':');
+
+                if (splitData.Length == 2  && float.TryParse(splitData[1], out result))
+                {
+                    InsertData(result);
+                }
+
                 //counter++;
             }
             catch (Exception ex)
             {
                 throw ex;
+            }
+        }
+
+        private static void InsertData(float data)
+        {
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var cmd = new SqlCommand();
+
+                cmd.CommandText = "InsertVibrationData";
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.Add("Velocity", SqlDbType.Decimal).Value = data;
+                cmd.Parameters.Add("Date", SqlDbType.DateTime2).Value = DateTime.Now;
+            
+
+                cmd.Connection = connection;
+
+                connection.Open();
+                Console.WriteLine("Connected successfully.");
+
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Insert successfull");
+
+                //Console.WriteLine("Press any key to finish...");
+                //Console.ReadKey(true);
             }
         }
     }
